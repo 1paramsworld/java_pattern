05d6dcd6f888d0a4a03d7efc3990b8d2647e4116
Param Shah <param.sanjay.shah@gmail.com>
2024-04-05 07:16:39 +0530

    changes done

--- pattern7.java
+++ pattern7.java
@@ -2,7 +2,8 @@ public class pattern7 {
     public static void main(String[] args){
         for(int i=1;i<=5;i++){
             for(int j=1;j<=i;j++){
-                if(j%2==1){
+                int sum=i+j;
+                if(sum%2==0){
                     System.out.print("1");
                 }
                 else{
