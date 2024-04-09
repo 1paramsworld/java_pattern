e7f1631954d042eb8a048ef3f414a530fbcfe202
Param Shah <param.sanjay.shah@gmail.com>
2024-04-09 15:29:00 +0530

    added

--- interview_1.java
+++ interview_1.java
@@ -1,95 +1,26 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
-/*
- 
- Kari Le Jate BKL ....... !!!!!!!!!!
-
- */
-/*
- 
- Kari Le Jate BKL ....... !!!!!!!!!!
-
- */
-/*
- 
- Kari Le Jate BKL ....... !!!!!!!!!!
-
- */
-/*
- 
- Kari Le Jate BKL ....... !!!!!!!!!!
-
- */
-/*
- 
- Kari Le Jate BKL ....... !!!!!!!!!!
-
- */
-/*
- 
- Kari Le Jate BKL ....... !!!!!!!!!!
-
- */
-/*
- 
- Kari Le Jate BKL ....... !!!!!!!!!!
-
- */
-
-
-
-
-
-
-// import java.lang.reflect.Array;
-// import java.util.Arrays;
-
-// public class interview_1{
-//     public static void main(String[] args){
-//         // int count=0;
-//         // int[] a={1,1,2,2,3,3,4,5,5,10};
-//         int[] a={1,2,4,5,6,7,1,4,3,2,8,8,9};
-//         Arrays.sort(a);
-//         System.out.println(Arrays.toString(a));
-//         System.out.println();
-//         for(int i=0;i<=a.length;i++){
-//             if (i+1 >= a.length){
-//                 System.out.println(a[i]);
-//                 break;
-//             }
-//             if(a[i]==a[i+1]){
-//                 i=i+1;
-//             }
-//             else{
-//                 System.out.println(a[i]);
-//             }
-//         }
-        
-//     }
-// }
-
-// import java.lang.reflect.Array;
-// import java.util.Arrays;
-
-// public class interview_1{
-//     public static void main(String[] args){
-//         int[] a={1,2,4,5,6,7,1,4,3,2,8,8,9};
-//         Arrays.sort(a);
-//         System.out.println(Arrays.toString(a));
-//         System.out.println();
-//         int i = 0;
-//         while(i<=a.length) {
-//             if (i+1 >= a.length){
-//                 System.out.println(a[i]);
-//                 break;
-//             }
-//             if(a[i]==a[i+1]){
-//                 i=i+2;
-//             }
-//             else{
-//                 System.out.println(a[i]);
-//                 i=i+1;
-//             }
-//         }      
-//     }
-// }
+public class interview_1{
+    public static void main(String[] args){
+        int[] a={1,2,4,5,6,7,1,4,3,2,8,8,9,9};
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+        System.out.println();
+        int i = 0;
+        while(i<=a.length) {
+            if (i+1 >= a.length){
+                System.out.println(a[i]);
+                break;
+            }
+            if(a[i]==a[i+1]){
+                i=i+2;
+            }
+            else{
+                System.out.println(a[i]);
+                i=i+1;
+            }
+        }      
+    }
+}
 
